62b0eb2b082c3c1a74ff722c8bfee2840a9f8880
safrapc <leandrosafra.jconectada@gmail.com>
2021-08-20 19:38:29 -0300

    finish fix moodle sidebar

---
+++ js/index.js
@@ -0,0 +1,14 @@
+//Start extension
+const start = () => {
+  fixSidebar();
+};
+
+//fix moodle sidebar
+const fixSidebar = () => {
+  const sidebar = document.querySelector("#nav-drawer > nav > ul");
+  for (let i = 0; i < sidebar.childElementCount; i++) {
+    sidebar.children[i].style.wordBreak = "break-word";
+    console.log("sidebar arrumada com sucesso");
+  }
+};
+start();
