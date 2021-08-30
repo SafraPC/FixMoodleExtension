127c2345902e2937edcf9fcc94313ad1c0537707
Leandro Safra <59981353+SafraPC@users.noreply.github.com>
2021-08-30 17:33:16 -0300

    remove js

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
-//Start extension
-const start = () => {
-  fixSidebar();
-};
+// //Start extension
+// const start = () => {
+//   fixSidebar();
+// };
 
-//fix moodle sidebar
-const fixSidebar = () => {
-  const sidebar = document.querySelector("#nav-drawer > nav > ul");
-  for (let i = 0; i < sidebar.childElementCount; i++) {
-    sidebar.children[i].style.wordBreak = "break-word";
-  }
-  const sidebarClicked = document.querySelector(
-    "#nav-drawer > nav.list-group.mt-1 > ul"
-  );
-  for (let i = 0; i < sidebarClicked.childElementCount; i++) {
-    sidebarClicked.children[i].style.wordBreak = "break-word";
-  }
-};
+// //fix moodle sidebar
+// const fixSidebar = () => {
+//   const sidebar = document.querySelector("#nav-drawer > nav > ul");
+//   for (let i = 0; i < sidebar.childElementCount; i++) {
+//     sidebar.children[i].style.wordBreak = "break-word";
+//   }
+//   const sidebarClicked = document.querySelector(
+//     "#nav-drawer > nav.list-group.mt-1 > ul"
+//   );
+//   for (let i = 0; i < sidebarClicked.childElementCount; i++) {
+//     sidebarClicked.children[i].style.wordBreak = "break-word";
+//   }
+// };
 
-start();
+// start();
